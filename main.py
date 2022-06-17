b1ec226e27a630a898331953fcf7d84a1735eca6
WangZechen <zechen_wang@qq.com>
2022-06-18 00:28:05 +0800

    debug: drop the columns

--- main.py
+++ main.py
@@ -10,16 +10,17 @@ def main():
     df.G3[(11 < df.G3) & (df.G3 <= 13)] = 3
     df.G3[(13 < df.G3) & (df.G3 <= 15)] = 4
     df.G3[df.G3 > 15] = 5
-    df.drop(columns=['G1', 'G2', 'absences'])
+    df.drop(columns=['G1', 'G2', 'absences', 'higher', 'school'], inplace=True)
     df = df.astype(str)
     for col in df.columns:
         df[col] = col + '_' + df[col]
 
-    L, suppData = lib.apriori(df.to_numpy(), minSupport=0.5)
+    L, suppData = lib.apriori(df.to_numpy(), minSupport=0.2)
     rules = lib.generateRules(L, suppData, minConf=0.9)
     rules = sorted(rules, key=lambda x: x[2], reverse=True)
     for i in rules:
         print(i)
+    # print(L)
 
 
 if __name__ == '__main__':
