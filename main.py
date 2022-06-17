de7c678e0f0d73061ec7d59ea1e7d3db22352ae5
WangZechen <zechen_wang@qq.com>
2022-06-17 23:00:59 +0800

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import lib
+
+
+def main():
+    df = pd.read_csv('./data/student-mat.csv')
+    df.G3[df.G3 <= 9] = 1
+    df.G3[(9 < df.G3) & (df.G3 <= 11)] = 2
+    df.G3[(11 < df.G3) & (df.G3 <= 13)] = 3
+    df.G3[(13 < df.G3) & (df.G3 <= 15)] = 4
+    df.G3[df.G3 > 15] = 5
+    df.drop(columns=['G1', 'G2', 'absences'])
+    df = df.astype(str)
+    for col in df.columns:
+        df[col] = col + '_' + df[col]
+
+    L, suppData = lib.apriori(df.to_numpy(), minSupport=0.5)
+    rules = lib.generateRules(L, suppData, minConf=0.9)
+    rules = sorted(rules, key=lambda x: x[2], reverse=True)
+    for i in rules:
+        print(i)
+
+
+if __name__ == '__main__':
+    main()
